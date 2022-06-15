7dd322f544a1f62ffc5ab12976c7f7b366282887
mateega <mateega@upenn.edu>
2022-06-15 10:15:35 -0700

    sign up skeleton

--- app/src/main/java/com/example/project/SignupActivity.java
+++ app/src/main/java/com/example/project/SignupActivity.java
@@ -3,13 +3,36 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etPhone;
+    EditText etPassword;
+
+    Button btnSignup;
+    Button btnFacebook;
+    Button btnGoogle;
+    Button btnLoginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         this.getSupportActionBar().hide();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPhone = findViewById(R.id.etPhone);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnSignup = findViewById(R.id.btnSignup);
+        btnFacebook = findViewById(R.id.btnFacebook);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        btnLoginText = findViewById(R.id.btnLoginText);
+
+        btnLoginText.setOnClickListener();
     }
 }
