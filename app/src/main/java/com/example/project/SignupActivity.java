dc4b371d1dda87723cff77c37a0fdfbebb6ad0ea
mateega <mateega@upenn.edu>
2022-06-20 12:01:50 -0700

    connect firestore to app

--- app/src/main/java/com/example/project/SignupActivity.java
+++ app/src/main/java/com/example/project/SignupActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +14,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
     private static final String TAG = "SIGN UP ACTIVITY";
+    EditText etName;
     EditText etEmail;
     //EditText etPhone;
     EditText etPassword;
@@ -32,6 +44,9 @@ public class SignupActivity extends AppCompatActivity {
     Button btnLoginText;
 
     private FirebaseAuth mAuth;
+    public static final String USERS = "users";
+    FirebaseFirestore db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +54,7 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         this.getSupportActionBar().hide();
 
+        etName = findViewById(R.id.etName);
         etEmail = findViewById(R.id.etEmail);
         //etPhone = findViewById(R.id.etPhone);
         etPassword = findViewById(R.id.etPassword);
@@ -55,18 +71,27 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = etName.getText().toString();
                 String email = etEmail.getText().toString();
                 //String phone = etPhone.getText().toString();
                 String password = etPassword.getText().toString();
-                signupUser(email, password);
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(email) || TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter name, email and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                User user = new User(name, email, password);
+                signupUser(name, email, password);
             }
         });
-
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
 
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -75,7 +100,7 @@ public class SignupActivity extends AppCompatActivity {
         }
     }
 
-    private void signupUser(String email, String password) {
+    private void signupUser(String name, String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -84,6 +109,8 @@ public class SignupActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                            addUserToFirestore(name, email, password);
                             goToGroups();
                             finish();
                         } else {
@@ -96,9 +123,28 @@ public class SignupActivity extends AppCompatActivity {
                     }
                 });
 
+    }
 
+    private void addUserToFirestore(String name, String email, String password) {
+        Map<String, Object> city = new HashMap<>();
+        city.put("name", name);
+        city.put("email", email);
+        city.put("password", password);
 
-
+        db.collection("users").document(email)
+                .set(city)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 
     private void goToGroups() {
---
+++ app/src/main/java/com/example/project/User.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+public class User {
+    String name;
+    String email;
+    String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
