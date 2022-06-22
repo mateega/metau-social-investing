cc5824ca1b25f23a9543130e60807c6f9fa3b5aa
mateega <mateega@upenn.edu>
2022-06-22 15:36:17 -0700

    read in data from coinAPI

--- app/src/main/java/com/example/project/fragments/TradeFragment.java
+++ app/src/main/java/com/example/project/fragments/TradeFragment.java
@@ -2,13 +2,34 @@ package com.example.project.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.project.BuildConfig;
 import com.example.project.R;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,10 +42,24 @@ public class TradeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "TRADE FRAGMENT";
+
+    EditText etSearch;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String searchText;
+
+    TextView tvCoinName;
+    TextView tvCoinTicker;
+    TextView tvPrice;
+    TextView tvPriceChange;
+
+    String name;
+    String ticker;
+    String price;
+    String priceChange;
 
     public TradeFragment() {
         // Required empty public constructor
@@ -63,4 +98,101 @@ public class TradeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trade, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvCoinName = view.findViewById(R.id.tvCoinName);
+        tvCoinTicker = view.findViewById(R.id.tvCoinTicker);
+        tvPrice = view.findViewById(R.id.tvPrice);
+        tvPriceChange = view.findViewById(R.id.tvPriceChange);
+
+
+        etSearch = view.findViewById(R.id.etSearch);
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                searchText = s.toString();
+                // Log.i(TAG, "Search: " + searchText);
+                if (searchText.isEmpty() || searchText.equals("")) {
+                    Log.i(TAG, "Search is empty");
+                } else {
+                    try {
+                        Log.i(TAG, "Text change");
+                        getAssetAPI(searchText);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+        });
+    }
+
+    private void getAssetAPI(String searchText) throws IOException {
+        name = "updated name";
+        setViews();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://rest.coinapi.io/v1/assets?filter_asset_id=" + searchText)
+                .get()
+                .addHeader(BuildConfig.COINAPI_AUTH_HEADER, BuildConfig.COINAPI_KEY)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                    // json response headers
+//                    Headers responseHeaders = response.headers();
+//                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+//                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+//                    }
+                    String jsonData = responseBody.string();
+                    try {
+                        JSONArray jsonArray = new JSONArray(jsonData);
+                        System.out.println("JSONARRAY SIZE: " + jsonArray.length());
+                        JSONObject object = jsonArray.getJSONObject(0);
+                        name = object.getString("name");
+                        ticker = object.getString("asset_id");
+                        price = object.getString("price_usd");
+                        priceChange = "N/A";
+                        setViews();
+
+                        Log.i(TAG, name);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void setViews() {
+        tvCoinName.setText(name);
+        tvCoinTicker.setText(ticker);
+        tvPrice.setText(price);
+        tvPriceChange.setText(priceChange);
+    }
+
 }
